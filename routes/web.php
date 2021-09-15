9ce7c2cea1b6a6039c06da43b1d8f1c176a2f85d
Diego <diegoantoniodasp@hotmail.com>
2021-09-15 18:21:36 -0300

    30. Super Gestão - Implementando as visualizações principal, sobre-nos e contato

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/sobre', [SobreNosController::class, 'sobre']);
+Route::get('/contato', [ContatoController::class, 'contato']);
