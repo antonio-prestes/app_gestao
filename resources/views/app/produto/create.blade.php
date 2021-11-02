e287aea3f93af20a222ba6468b6385e046d925f2
Diego <diegoantoniodasp@hotmail.com>
2021-11-02 17:45:24 -0300

    171. Modificando a forma de edição de registros parte 2

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="titulo-pagina-app">
-        <p>Produto - Adicionar</p>
+            <p>Produto - Adicionar</p>
     </div>
     <div class="menu">
         <ul>
@@ -14,33 +14,8 @@
     </div>
     <div class="informacao-pagina">
         <div style="width: 30%; margin-left: auto; margin-right: auto">
-            <form action="{{route('produto.store')}}" method="post">
-                @csrf
-                <input type="text" name="nome" value="{{old('nome')}}" placeholder="Nome"
-                       class="borda-preta"/>
-                {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-                <input type="text" name="descricao" value="{{old('descricao')}}" placeholder="Descrição"
-                       class="borda-preta"/>
-                {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                <select name="unidade_id">
-                    <option value="">Selecione a unidade de medida</option>
-                    @foreach($unidades as $unidade)
-                        <option value="{{$unidade->id}}">{{$unidade->descricao}}</option>
-                    @endforeach
-                </select>
-
-                <input type="number" name="peso" value="{{old('peso')}}" placeholder="Peso"
-                       class="borda-preta"/>
-                {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-                <input type="number" name="estoque_minimo" value="{{old('estoque_minimo')}}" placeholder="Estoque Minimo"
-                       class="borda-preta">
-                {{ $errors->has('estoque_minimo') ? $errors->first('estoque_minimo') : '' }}
-                <input type="number" name="estoque_maximo" value="{{old('estoque_maximo')}}" placeholder="Estoque Máximo"
-                       class="borda-preta">
-                {{ $errors->has('estoque_maximo') ? $errors->first('estoque_maximo') : '' }}
-                <button type="submit">Cadastrar</button>
-            </form>
+            @component('app.produto._components.form_create_edit', ['unidades' => $unidades])
+            @endcomponent
         </div>
     </div>
 @endsection
--- resources/views/app/produto/edit.blade.php
+++ resources/views/app/produto/edit.blade.php
@@ -14,37 +14,8 @@
     </div>
     <div class="informacao-pagina">
         <div style="width: 30%; margin-left: auto; margin-right: auto">
-            <form action="{{route('produto.update', ['produto'=>$produto->id])}}" method="post">
-                @csrf
-                @method('PUT')
-                <input type="text" name="nome" value="{{$produto->nome ?? old('nome')}}" placeholder="Nome"
-                       class="borda-preta"/>
-                {{ $errors->has('nome') ? $errors->first('nome') : '' }}
-                <input type="text" name="descricao" value="{{$produto->descricao ?? old('descricao')}}"
-                       placeholder="Descrição"
-                       class="borda-preta"/>
-                {{ $errors->has('descricao') ? $errors->first('descricao') : '' }}
-
-                <select name="unidade_id">
-                    <option value="">Selecione a unidade de medida</option>
-                    @foreach($unidades as $unidade)
-                        <option value="{{$unidade->id}}">{{$unidade->descricao}} </option>
-                    @endforeach
-                </select>
-
-                <input type="number" name="peso" value="{{$produto->peso ?? old('peso')}}" placeholder="Peso"
-                       class="borda-preta"/>
-                {{ $errors->has('peso') ? $errors->first('peso') : '' }}
-                <input type="number" name="estoque_minimo" value="{{$produto->estoque_minimo ?? old('estoque_minimo')}}"
-                       placeholder="Estoque Minimo"
-                       class="borda-preta">
-                {{ $errors->has('estoque_minimo') ? $errors->first('estoque_maximo') : '' }}
-                <input type="number" name="estoque_maximo" value="{{$produto->estoque_maximo ?? old('estoque_maximo')}}"
-                       placeholder="Estoque Máximo"
-                       class="borda-preta">
-                {{ $errors->has('estoque_maximo') ? $errors->first('estoque_maximo') : '' }}
-                <button type="submit">Cadastrar</button>
-            </form>
+            @component('app.produto._components.form_create_edit', ['produto' => $produto, 'unidades' => $unidades])
+            @endcomponent
         </div>
     </div>
 @endsection
