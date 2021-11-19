3a44f07d17917cc4a10a38c0130f317f55c2aecf
Diego <diegoantoniodasp@hotmail.com>
2021-11-19 18:36:23 -0300

    189. Implementando a tela de listagem de clientes

--- resources/views/app/cliente.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app.layouts.basic')
-
-@section('title', 'Cliente')
-
-@section('content')
-<br><br><br><br><br><br>Cliente
-@endsection
---
+++ resources/views/app/clientes/index.blade.php
@@ -0,0 +1,54 @@
+@extends('app.layouts.basic')
+
+@section('title', 'Cliente')
+
+@section('content')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina-app">
+            <p>Clientes</p>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="{{route('cliente.create')}}">Novo</a></li>
+                <li><a href="">Consulta</a></li>
+            </ul>
+        </div>
+        <div class="informacao-pagina">
+            <div style="width: 90%; margin-left: auto; margin-right: auto">
+                <table border="1" width="100%">
+                    <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($clientes as $cliente)
+                        <tr>
+                            <td>{{$clientes->nome}}</td>
+
+                            <td><a href="{{ route('cliente.show',['cliente'=>$cliente->id]) }}">Vizualizar</a></td>
+                            <td><a href="{{ route('cliente.edit', ['cliente'=>$cliente->id]) }}">Editar</a></td>
+                            <td>
+                                <form id="form_{{$cliente->id}}"
+                                      method="post"
+                                      action="{{ route('cliente.destroy', ['cliente'=>$cliente->id]) }}">
+                                    @method('DELETE')
+                                    @csrf
+                                    <a href="#" onclick="document.getElementById('form_{{$cliente->id}}').submit()">Excluir</a>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+
+                </table>
+                {{ $clientes->appends($request)->links() }}
+                <br>
+                Exibindo {{$clientes->count()}} fornecedores de {{$clientes->total()}}
+                ({{$clientes->firstItem()}} a {{$clientes->lastItem()}})
+            </div>
+        </div>
+    </div>
+@endsection
